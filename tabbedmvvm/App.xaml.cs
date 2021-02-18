57be8d52f4b932e679e669f037da53b8ce8e2917
Helkin-Chacon <63528430+Helkin-Chacon@users.noreply.github.com>
2021-02-17 22:02:41 -0500

    mvvmtab

---
+++ tabbedmvvm/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using tabbedmvvm.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tabbedmvvm
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new TabView();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ tabbedmvvm/viewModel/TabViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.XForms.TabView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace tabbedmvvm.viewModel
+{
+	class TabViewModel : NotificationObject
+	{
+		private TabItemCollection tabItems = new TabItemCollection();
+
+		SfTabView TabPage;
+		public TabItemCollection TabItems
+		{
+			get { 
+				return this.tabItems;
+			}
+			set {
+				this.tabItems = value;
+				OnPropertyChanged();
+			}
+		}
+		public TabViewModel()
+		{
+			SfTabItem item1 = new SfTabItem();
+			item1.Title = "home";
+			item1.HeaderContent =
+				new Grid
+				{
+					GestureRecognizers =
+					{
+						new TapGestureRecognizer { Command = GoTo1() }
+					}
+
+				
+			}
+		
+				TabPage.Items = tabItems;
+
+
+			
+		}
+
+		private void GoTo1()
+		{
+			TabPage.SelectedIndex = 0;
+		}
+
+
+
+	}
+}
